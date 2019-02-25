dc52d26b44bfe6f4fcaf0cee26aa029f2071be19
ednakaha <ednakaha@gmail.com>
2019-02-25 20:43:59 +0200

    first

---
+++ index.js
@@ -0,0 +1,41 @@
+
+// const app = require('express')();
+const express = require('express');
+const cors = require('cors');
+const PORT = 8888;
+const app = express();
+app.use(cors());
+
+
+
+
+
+app.get('/student', function (req, res) {
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'students'
+    });
+
+    connection.connect();
+
+
+    connection.query('SELECT * from students', function (error, results, fields) {
+        if (error) throw error;
+        res.send(results);
+    });
+    connection.end();
+
+});
+
+app.post('/', function (req, res) {
+    res.send('post home');
+});
+
+
+
+app.listen(PORT, function () {
+    console.log('server started at port ' + PORT)
+});
